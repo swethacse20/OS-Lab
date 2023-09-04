91c138f7fab6f969102965e61f32b79d308ae37a
swethacse20 <76720444+swethacse20@users.noreply.github.com>
2023-09-04 15:45:44 +0530

    Add files via upload

---
+++ week5/pipes.c
@@ -0,0 +1,51 @@
+/*SYSTEM CALL: pipe();                                                          
+
+  PROTOTYPE: int pipe( int fd[2] );                                             
+    RETURNS: 0 on success                                                       
+             -1 on error: errno = EMFILE (no free descriptors)                  
+                                  EMFILE (system file table is full)            
+                                  EFAULT (fd array is not valid)                
+
+  NOTES: fd[0] is set up for reading, fd[1] is set up for writing
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+        int     fd[2], nbytes;
+        pid_t   childpid;
+        char    string[] = "Hello, world!\n";
+        char    readbuffer[80];
+
+        pipe(fd);
+        
+        if((childpid = fork()) == -1)
+        {
+                perror("fork");
+                exit(1);
+        }
+
+        if(childpid == 0)
+        {
+                /* Child process closes up input side of pipe */
+                close(fd[0]);
+
+                /* Send "string" through the output side of pipe */
+                write(fd[1], string, (strlen(string)+1));
+                exit(0);
+        }
+        else
+        {
+                /* Parent process closes up output side of pipe */
+                close(fd[1]);
+
+                /* Read in a string from the pipe */
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+                printf("Received string: %s", readbuffer);
+        }
+        
+        return(0);
+}
